845304b74fca23f5cabcdf0830621ffd03b78b00
bendking <ben.shabtai@gmail.com>
2025-08-01 14:03:54 +0300

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  plugins: [
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": "/src",
     },
   },
-})
+});
