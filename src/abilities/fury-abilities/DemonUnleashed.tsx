2f34fbacaab7b8c68fc49250983b7f29a4fd508d
bendking <ben.shabtai@gmail.com>
2026-02-28 14:56:33 +0200

    Update Demon Unleashed

--- src/abilities/fury-abilities/DemonUnleashed.tsx
+++ src/abilities/fury-abilities/DemonUnleashed.tsx
@@ -6,14 +6,13 @@ export const DemonUnleashed = () => (
     subtitle="Foes tremble at the sight of you."
     keywords="Magic"
     actionType="Maneuver"
-    range="Self"
+    range="2 Burst"
     targets="Self"
     effect={
       <Text>
-        <strong>Effect:</strong> Until the end of the encounter or until you are
-        dying, each enemy who starts their turn within range 3 of you and has{" "}
-        <Highlight>{"P < STRONG"}</Highlight> is frightened until the end of
-        their turn.
+        <strong>Effect:</strong> Each target moves half their speed away from
+        you, and is <Highlight>{"I < AVERAGE"}</Highlight> frightened until the
+        end of their next turn.
       </Text>
     }
   />
